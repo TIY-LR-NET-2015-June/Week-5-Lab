34128b5812fe9fdc273ff6b39b8ddceffff6b104
Michael Null <michael.null@bpmw.net>
2015-08-01 22:44:40 -0500

    startover

---
+++ MikeIt/MikeIt/Models/Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MikeIt.Models
+{
+    public class Post
+    {
+       
+        public int Id { get; set; }
+
+        [Required]
+        public string Headline { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        [Required]
+        public DateTime SubmittedOn { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+    }
+    public class PostContext : DbContext
+    {
+        public virtual DbSet<Post> Posts { get; set; }
+    }
+}
